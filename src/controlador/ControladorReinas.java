3ba392c3f475d0db550ee2dd8df5e1fccbc43b68
Valenbu <valentina123acosta@gmail.com>
2025-05-11 20:27:42 -0500

    se agregan los comandos en el controalador de las reinas

--- src/controlador/ControladorReinas.java
+++ src/controlador/ControladorReinas.java
@@ -1,5 +1,21 @@
 package controlador;
 
+import modelo.Tablero;
+import vista.VistaTablero;
+
+// Controlador que maneja la lógica entre modelo y vista
 public class ControladorReinas {
-    
+    private Tablero modelo;
+    private VistaTablero vista;
+
+    public ControladorReinas(Tablero modelo, VistaTablero vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public void mejecutar(int fila, int columna) {
+        modelo.mcolocarPrimeraReina(fila, columna);
+        modelo.mresolverDesde(fila + 1, columna);
+        vista.mmostrarTodas(modelo.mobtenerSoluciones());
+    }
 }
